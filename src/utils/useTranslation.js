2246dac02e9293aa50765574ffcf6c81f05e8a14
dastasoft <dastasoft@gmail.com>
2020-08-02 13:10:57 +0200

    use-translation@0.0.2

--- src/utils/useTranslation.js
+++ src/utils/useTranslation.js
@@ -1,23 +1,31 @@
 import { useState } from 'react';
 
-const useTranlation = (appLanguage, appDictionary) => {
+const useTranlation = (appLanguage = 'en', appDictionary = {}) => {
   const [dictionary] = useState(appDictionary);
   const [language, setLanguage] = useState(appLanguage);
 
   const changeLanguage = newLanguage => setLanguage(newLanguage);
 
-  const getTranslation = (text, replacements) => {
+  const getTranslation = (text = '', replacements = []) => {
     const register = dictionary[text];
     if (typeof register === 'undefined') return text;
 
     let translation = register[language];
-    if (replacements)
-      replacements.forEach(replacement => {
+    if (replacements) {
+      if (Array.isArray(replacements)) {
+        replacements.forEach(replacement => {
+          translation = translation.replace(
+            `%${replacement.label}%`,
+            replacement.value
+          );
+        });
+      } else if (replacements.label) {
         translation = translation.replace(
-          `%${replacement.label}%`,
-          replacement.value
+          `%${replacements.label}%`,
+          replacements.value
         );
-      });
+      }
+    }
 
     return translation || text;
   };
